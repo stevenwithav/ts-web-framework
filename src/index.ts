03a264de58b24f2705470236f1abbb33be0faea6
nettlesoup <sanettleingham@hotmail.co.uk>
2020-10-09 15:28:54 +0100

    CollectionView to show user list example

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
-import { UserEdit } from './views/UserEdit';
-import { User } from './models/User';
+// import { UserEdit } from './views/UserEdit';
+// import { User } from './models/User';
 
-const user = User.createUser({ name: 'Name', age: 40 });
+// const user = User.createUser({ name: 'Name', age: 40 });
 
-const root = document.getElementById('root');
-if (root) {
-	const userEdit = new UserEdit(root, user);
+// const root = document.getElementById('root');
+// if (root) {
+// 	const userEdit = new UserEdit(root, user);
 
-	userEdit.render();
+// 	userEdit.render();
 
-	console.log(userEdit);
-} else {
-	throw new Error('Root element not found');
-}
+// 	console.log(userEdit);
+// } else {
+// 	throw new Error('Root element not found');
+// }
+
+import { UserList } from './views/UserList';
+import { Collection } from './models/Collection';
+import { User, UserProps } from './models/User';
+
+const users = new Collection(
+	'http://localhost:3000/users',
+	(json: UserProps) => {
+		return User.createUser(json);
+	}
+);
+
+users.on('change', () => {
+	const root = document.getElementById('root');
+
+	if (root) {
+		new UserList(root, users).render();
+	}
+});
+
+users.fetch();
